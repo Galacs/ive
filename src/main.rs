04eeb5b82c17bc55d62d32786235a05931506439
Rémi Ait-Younes <remi@ait-younes.fr>
2022-11-20 11:57:11 +0100

    Check for tmpfs dir on start

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod commands;
 mod utils;
 
 use std::env;
+use std::path::Path;
 
 use serenity::async_trait;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
@@ -9,6 +10,7 @@ use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
 use serenity::model::prelude::command::{Command, CommandType};
 use serenity::prelude::*;
+use tokio::fs::create_dir;
 
 struct Handler;
 
@@ -72,6 +74,13 @@ impl EventHandler for Handler {
 async fn main() {
     // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    // Create tmp folder
+    if !Path::new("tmpfs").exists() {
+        if let Err(why) = create_dir("tmpfs").await {
+            panic!("Can't create tmp dir: {}", why);
+        }
+    }
 
     // Build our client.
     let mut client = Client::builder(token, GatewayIntents::empty())
