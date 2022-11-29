7ecc15c6292ca8b15d223dd17be2740caa93f60f
Rémi Ait-Younes <remi@ait-younes.fr>
2022-11-29 19:18:37 +0100

    better error recovery for worker

--- lib/worker/src/main.rs
+++ lib/worker/src/main.rs
@@ -1,9 +1,71 @@
 use std::path::Path;
 
+use models::{EncodeParameters, Job, JobProgress, InteractionError};
 use queue::Queue;
-use models::{Job, EncodeParameters, JobProgress};
-use redis::Commands;
+use redis::{Client, Commands};
 use tokio::fs;
+
+#[derive(Debug)]
+enum ProcessError {
+    NoVideo,
+    File(std::io::Error),
+    Serde(serde_json::Error),
+    Redis(redis::RedisError),
+    Error,
+}
+
+impl From<std::io::Error> for ProcessError {
+    fn from(error: std::io::Error) -> Self {
+        ProcessError::File(error)
+    }
+}
+
+impl From<serde_json::Error> for ProcessError {
+    fn from(error: serde_json::Error) -> Self {
+        ProcessError::Serde(error)
+    }
+}
+
+impl From<redis::RedisError> for ProcessError {
+    fn from(error: redis::RedisError) -> Self {
+        ProcessError::Redis(error)
+    }
+}
+
+impl From<InteractionError> for ProcessError {
+    fn from(_: InteractionError) -> Self {
+        ProcessError::Error
+    }
+}
+
+async fn process_job(job: Job, client: &mut Client) -> Result<(), ProcessError> {
+    let video = job.video.ok_or(ProcessError::NoVideo)?;
+
+    // Define working directory and destination filepath
+    let dir = Path::new("tmpfs").join(format!("{}", &video.id));
+    let dir = std::env::current_dir()?.join(dir);
+
+    // Creating working directory
+    fs::create_dir(&dir).await?;
+
+    let channel = format!("progress:{}", video.id);
+
+    let str = serde_json::to_string(&JobProgress::Started)?;
+    let _: () = client.publish(&channel, str)?;
+
+    let params = match &job.params {
+        EncodeParameters::EncodeToSize(p) => p,
+    };
+
+    let _ = ffedit::encoding::encode_to_size_new(&video, params).await;
+
+    let dir = ffedit::encoding::get_working_dir(&video.id)?;
+    tokio::fs::remove_dir_all(dir).await?;
+
+    let str = serde_json::to_string(&JobProgress::Done)?;
+    let _: () = client.publish(&channel, str)?;
+    Ok(())
+}
 
 #[tokio::main]
 
@@ -12,33 +74,16 @@ async fn main() {
     let mut con = client.get_async_connection().await.unwrap();
 
     loop {
-        let a = Job::receive_job(&mut con).await.unwrap();
-        let video = a.video.unwrap();
-
-        // Define working directory and destination filepath
-        let dir = Path::new("tmpfs").join(format!("{}", &video.id));
-        let dir = std::env::current_dir().unwrap().join(dir);
-        let dest_file = dir.join(&format!("edit-{}", &video.filename));
-
-        // Creating working directory
-        fs::create_dir(&dir).await.unwrap();
-
-        let channel = format!("progress:{}", video.id);
-
-        let str = serde_json::to_string(&JobProgress::Started).unwrap();
-        let _ : () = client.publish(&channel, str).unwrap();
-
-        let params = match &a.params {
-            EncodeParameters::EncodeToSize(p) => p,
+        let job = Job::receive_job(&mut con).await;
+        let job = match job {
+            Ok(j) => j,
+            Err(err) => {
+                println!("{:?}", err);
+                continue;
+            }
         };
-
-        let _ = ffedit::encoding::encode_to_size_new(&video, params).await;
-
-        let dir = ffedit::encoding::get_working_dir(&video.id).unwrap();
-        tokio::fs::remove_dir_all(dir).await.unwrap();
-
-        let str = serde_json::to_string(&JobProgress::Done).unwrap();
-        let _ : () = client.publish(&channel, str).unwrap();
+        if let Err(why) = process_job(job, &mut client).await {
+            println!("Processing error: {:?}", why);
+        }
     }
-
 }
